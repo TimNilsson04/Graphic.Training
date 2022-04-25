a4ec18799e90a0be7c2de2927318f4dd5e695ee4
TimNilsson04 <tim.nilsson2@elev.ga.ntig.se>
2022-04-25 11:18:45 +0200

    Försökte röra huset med hjälp av a,d tangenterna

--- src/Picture.java
+++ src/Picture.java
@@ -1,5 +1,11 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * This is a class
@@ -7,9 +13,13 @@ import java.awt.*;
  *
  * @author Magnus Silverdal
  */
-public class Picture extends Canvas {
+public class Picture extends  Canvas implements Runnable{
     private final int height = 600;
     private final int width = 800;
+    private boolean isRunning;
+    int fps = 30;
+    private Thread thread;
+    private BufferStrategy bs;
     int treeX = 200;
     int treeY = 200;
     int cloudX = 100;
@@ -18,10 +28,11 @@ public class Picture extends Canvas {
     int sunY = 50;
     int sunW = 100;
     int sunH = 100;
-    int x = 400;
     int[] mountainY= {250,150,250};
     int[] mountainX= {150,400,750};
     int houseX = 500;
+    int houseVX = 0;
+
 
     public Picture() {
         JFrame frame = new JFrame("Picture");
@@ -30,22 +41,58 @@ public class Picture extends Canvas {
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+        isRunning = false;
+
+        frame.addKeyListener(new KL());
+
+
+    }
+    public synchronized void start() {
+        thread = new Thread(this);
+        isRunning = true;
+        thread.start();
+    }
+    public synchronized void stop() {
+        isRunning = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void paint() {
+        bs = getBufferStrategy();
+        if (bs == null) {
+            createBufferStrategy(3);
+            return;
+        }
+        Graphics g = bs.getDrawGraphics();
+
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, width, height);
+        draw(g);
+        g.dispose();
+        bs.show();
+    }
+    private void update() {
+        if (houseX + houseVX < width+4*marioimg.getWidth() && houseX + houseVX > 0)
+            houseX += houseVX;
+
     }
 
     public void paint(Graphics g) {
         sunY += 1;
         cloudX += 5;
-        treeX += 5;
-        houseX += 5;
+        treeX += 2;
+
 
         if(sunY > 200)
             sunY = 0;
         if (cloudX > 850)
             cloudX = 0;
-        if (houseX > 850)
-            houseX = 0;
         if (treeX > 850)
-            treeX = 0;
+            treeX = -100;
 
         draw(g);
         repaint();
@@ -57,16 +104,17 @@ public class Picture extends Canvas {
     }
 
 
+
     public void draw(Graphics g) {
         drawsky(g);
         drawsun(g);
         drawgrass(g);
         drawmountain(g);
-        drawhouse(g);
         drawcloud(g);
         drawTree(g, treeX,treeY);
         drawTree(g, treeX+30,treeY);
         drawTree(g, treeX+60,treeY);
+        drawhouse(g);
     }
 
     private void drawTree(Graphics g, int x, int y) {
@@ -120,5 +168,61 @@ public class Picture extends Canvas {
     public static void main(String[] args) {
         Picture exempel = new Picture();
     }
+
+    @Override
+    public void run() {
+        double deltaT = 1000.0 / fps;
+        long lastTime = System.currentTimeMillis();
+
+        while (isRunning) {
+            long now = System.currentTimeMillis();
+            if (now - lastTime > deltaT) {
+                update();
+                lastTime = now;
+            }
+            paint();
+        }
+        stop();
+    }
+
+    private class KL implements KeyListener {
+        @Override
+        public void keyTyped(KeyEvent keyEvent) {
+
+        }
+
+        @Override
+        public void keyPressed(KeyEvent keyEvent) {
+            if (keyEvent.getKeyChar() == 'a') {
+                houseVX = -5;
+            }
+            if (keyEvent.getKeyChar() == 'd') {
+                houseVX = 5;
+            }
+            if (keyEvent.getKeyChar() == 'w') {
+                houseVX = 0;
+            }
+            if (keyEvent.getKeyChar() == 's') {
+                houseVX = 0;
+            }
+        }
+
+        @Override
+        public void keyReleased(KeyEvent keyEvent) {
+            if (keyEvent.getKeyChar() == 'a') {
+                houseVX = 0;
+            }
+            if (keyEvent.getKeyChar() == 'd') {
+                houseVX = 0;
+            }
+            if (keyEvent.getKeyChar() == 'w') {
+                houseVX = 0;
+            }
+            if (keyEvent.getKeyChar() == 's') {
+                houseVX = 0;
+            }
+        }
+    }
 }
+
 
